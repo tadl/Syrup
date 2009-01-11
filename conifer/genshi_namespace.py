62391d310517e10eabcd6bf44ea9e3527335e722
gfawcett <gfawcett@6d9bc8c9-1ec2-4278-b937-99fde70a366f>
2009-01-11 02:11:40 +0000

    added function to derive URLs for courses
    git-svn-id: svn://svn.open-ils.org/ILS-Contrib/servres/trunk@100 6d9bc8c9-1ec2-4278-b937-99fde70a366f

--- conifer/genshi_namespace.py
+++ conifer/genshi_namespace.py
@@ -4,8 +4,14 @@
 # rendering a Genshi template.
 
 from itertools import cycle
-
 from conifer.syrup import models
+
+# Root-relative URLs Django has its own way of doing this, by doing
+# reverse lookups in urlpatterns. Is there a benefit to their
+# approach?
 
 def item_url(item, suffix=''):
     return '/syrup/course/%d/item/%d/%s' % (item.course_id, item.id, suffix)
+
+def course_url(course, suffix=''):
+    return '/syrup/course/%d/%s' % (course.id, suffix)
