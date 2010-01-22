ffac1a804ff616634e43d174cc783c15b4af2605
gfawcett <gfawcett@6d9bc8c9-1ec2-4278-b937-99fde70a366f>
2010-01-22 03:20:58 +0000

    fixed title-lookup during edit-course
    The buggy version had a bad hard-coded prefix in the lookup-title
    URL. I've added a ROOT variable in JavaScript, set to the URL prefix
    (the SCRIPT_NAME) of the Syrup instance; and now we use this to
    correctly find the lookup-title service.
    
    git-svn-id: svn://svn.open-ils.org/ILS-Contrib/servres/trunk@763 6d9bc8c9-1ec2-4278-b937-99fde70a366f

--- conifer/static/edit_course.js
+++ conifer/static/edit_course.js
@@ -1,12 +1,9 @@
-/*
-this seems to be causing a disable when we don't want it
-*/
 function do_init() {
-    if ($('#id_code')[0].tagName == 'SELECT') {
+    if ($('#id_code').attr('tagName') == 'SELECT') {
 	// code is a SELECT, so we add a callback to lookup titles.
 	$('#id_code').change(function() {
 	    $('#id_title')[0].disabled=true;
-	    $.getJSON('/syrup/course/new/ajax_title', {course_code: $(this).val()},
+	    $.getJSON(ROOT + '/course/new/ajax_title', {course_code: $(this).val()},
 		      function(resp) {
 			  $('#id_title').val(resp.title)
 			  $('#id_title')[0].disabled=false;
