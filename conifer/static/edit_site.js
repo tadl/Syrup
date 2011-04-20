369942c544fce02f0b53161ce02bef1bd50f73dc
gfawcett <gfawcett@6d9bc8c9-1ec2-4278-b937-99fde70a366f>
2011-04-20 02:43:36 +0000

    site add/edit: when setting the start term, if end term is blank, set it to the same value.
    git-svn-id: svn://svn.open-ils.org/ILS-Contrib/servres/trunk@1404 6d9bc8c9-1ec2-4278-b937-99fde70a366f

--- conifer/static/edit_site.js
+++ conifer/static/edit_site.js
@@ -1,4 +1,11 @@
 $(function() {
     var fuzzy = fuzzyFinder(
 	'#fuzzyinput', '#fuzzypanel', '#fuzzyedit', '#fuzzyview', '#fuzzyname', '#fuzzychange', '#owner');
+    $('#id_start_term').change(onStartTermChange);
 });
+
+function onStartTermChange() {
+    if ($('#id_end_term').val() == "") {
+	$('#id_end_term').val($('#id_start_term').val());
+    }
+}
