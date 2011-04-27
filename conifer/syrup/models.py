42fd4249c03ddb8cdebee789046af9989be67156
artunit <artunit@6d9bc8c9-1ec2-4278-b937-99fde70a366f>
2011-04-27 20:23:13 +0000

    model field added for course notes, no template entries yet
    git-svn-id: svn://svn.open-ils.org/ILS-Contrib/servres/trunk@1426 6d9bc8c9-1ec2-4278-b937-99fde70a366f

--- conifer/syrup/models.py
+++ conifer/syrup/models.py
@@ -298,6 +298,8 @@ class Site(BaseModel):
                          default=ACCESS_DEFAULT,
                          choices=ACCESS_CHOICES)
 
+    sitenotes = m.TextField('Course Site Notes', blank=True, null=True)
+
     # temporary, uwindsor-only fields! For initial migration. FIXME: remove.
     uwindsor_bookbag = m.CharField(max_length=2048, blank=True, null=True)
     uwindsor_eres = m.CharField(max_length=2048, blank=True, null=True)
